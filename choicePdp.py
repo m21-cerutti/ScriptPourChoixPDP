aad9ea5da274c7f2a3af01d42f13aa67444d1aa9
m21-cerutti <marc.cerutti@outlook.fr>
2020-01-09 22:18:33 +0100

    Add files via upload

--- choicePdp.py
+++ choicePdp.py
@@ -9,7 +9,7 @@ order = []
 def sortByPref(val): 
     return val[2]  
 
-def addToListOrdered (numberList, description, orderPref):
+def addToListOrdered (numberList, description, orderPref = None):
     if(orderPref == None):
         orderPref = random.randint(MaxList, 100)
 
@@ -24,31 +24,31 @@ def addToListOrdered (numberList, description, orderPref):
 
     
 
-addToListOrdered( 1     ,"FP tutor    		                                                   ", None)
-addToListOrdered( 2     ,"Tuiles multimédia 3D sur le web                                      ", None)
-addToListOrdered( 3     ,"Algorithmes pour gagner à Hanabi                                     ", None)
-addToListOrdered( 4     ,"Planification de mouvement pour robots                               ", None)
-addToListOrdered( 5     ,"Clicodrome pour lexique électronique                                 ", None)
-addToListOrdered( 6     ,"Application météo décalée pour Android                               ", None)
-addToListOrdered( 7     ,"Spatialisation par le geste                                          ", None)
-addToListOrdered( 8     ,"Visualisations de traces d'exécution                                 ", None)
-addToListOrdered( 9     ,"Développement sur radio-logicielle d'une passerelle                  ", None)
-addToListOrdered( 10    ,"Environement Amstrad CPC pour la bibliothèque d'apprenti             ", None)
-addToListOrdered( 11    ,"Génération de playlistes musicales pour un groupe d'utilis           ", None)
-addToListOrdered( 12    ,"Les processus de décision                                            ", None)
-addToListOrdered( 13    ,"Localisation de robots en présence de faux positif                   ", None)
-addToListOrdered( 14    ,"Suivi de robots pour annotation semi-automatiq                       ", None)
-addToListOrdered( 15    ,"Création et dégradation d'images pour une utilisation en dee         ", None)
-addToListOrdered( 16    ,"Analyse d'architecture logicielles                                   ", None)
-addToListOrdered( 17    ,"Analyse d'architecture logicielles                                   ", None)
-addToListOrdered( 18    ,"Slitherlink                                                          ", None)
-addToListOrdered( 19    ,"Environnement d'exécution y86+HCL en Python ou en Node               ", None)
-addToListOrdered( 20    ,"Joueur de Scrabble                                                   ", None)
-addToListOrdered( 21    ,"Génération procédurale de planètes sphériques                        ", None)
-addToListOrdered( 22    ,"IA pour un jeu de Capture the Flag temps réel en Pyt                 ", None)
-addToListOrdered( 23    ,"Personal Hypno Vibes 2 (pdf)                                         ", None)
-addToListOrdered( 24    ,"Visualisation de tâches hiérarchiques (pdf)                          ", None)
-addToListOrdered( 25    ,"Calcul de cycles hamiltoniens dans des graphes fullerènes            ", None)
+addToListOrdered( 1     ,"FP tutor    		                                                   ")
+addToListOrdered( 2     ,"Tuiles multimédia 3D sur le web                                      ")
+addToListOrdered( 3     ,"Algorithmes pour gagner à Hanabi                                     ")
+addToListOrdered( 4     ,"Planification de mouvement pour robots                               ")
+addToListOrdered( 5     ,"Clicodrome pour lexique électronique                                 ")
+addToListOrdered( 6     ,"Application météo décalée pour Android                               ")
+addToListOrdered( 7     ,"Spatialisation par le geste                                          ")
+addToListOrdered( 8     ,"Visualisations de traces d'exécution                                 ")
+addToListOrdered( 9     ,"Développement sur radio-logicielle d'une passerelle                  ")
+addToListOrdered( 10    ,"Environement Amstrad CPC pour la bibliothèque d'apprenti             ")
+addToListOrdered( 11    ,"Génération de playlistes musicales pour un groupe d'utilis           ")
+addToListOrdered( 12    ,"Les processus de décision                                            ")
+addToListOrdered( 13    ,"Localisation de robots en présence de faux positif                   ")
+addToListOrdered( 14    ,"Suivi de robots pour annotation semi-automatiq                       ")
+addToListOrdered( 15    ,"Création et dégradation d'images pour une utilisation en dee         ")
+addToListOrdered( 16    ,"Analyse d'architecture logicielles                                   ")
+addToListOrdered( 17    ,"Analyse d'architecture logicielles                                   ")
+addToListOrdered( 18    ,"Slitherlink                                                          ")
+addToListOrdered( 19    ,"Environnement d'exécution y86+HCL en Python ou en Node               ")
+addToListOrdered( 20    ,"Joueur de Scrabble                                                   ")
+addToListOrdered( 21    ,"Génération procédurale de planètes sphériques                        ")
+addToListOrdered( 22    ,"IA pour un jeu de Capture the Flag temps réel en Pyt                 ")
+addToListOrdered( 23    ,"Personal Hypno Vibes 2 (pdf)                                         ")
+addToListOrdered( 24    ,"Visualisation de tâches hiérarchiques (pdf)                          ")
+addToListOrdered( 25    ,"Calcul de cycles hamiltoniens dans des graphes fullerènes            ")
 
 order.sort(key = sortByPref)
 
